11a93f23c3a43576c5f1d062efbc12dc7b656053
jayeen28 <jayeen6783@gmail.com>
2024-07-11 19:51:52 +0600

    single comment box shown with reactions and reply buttons

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -3,8 +3,8 @@ import styles from './avatar.module.scss';
 
 const Avatar = ({ id, ...rest }) => {
     return (
-        <div className={styles.avatar_wrapper}>
-            <img className={styles.avatar} src={`${process.env.REACT_APP_BACKEND_URL}/file/${id}`} alt='Avatar' {...rest} />
+        <div className={styles.avatar_wrapper} {...rest}>
+            <img className={styles.avatar} src={`${process.env.REACT_APP_BACKEND_URL}/file/${id}`} alt='Avatar' />
         </div>
     );
 }
--- src/features/Post/components/Comment.js
+++ src/features/Post/components/Comment.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import styles from '../styles/comment.module.scss';
+import Avatar from '../../../components/Avatar/Avatar';
 
-const Comment = ({ commentId }) => {
+const Comment = ({ comment }) => {
     return (
-        <div>
-            {commentId}
+        <div className={styles.comment_box_wrapper}>
+            <div>
+                <Avatar id={comment.user.avatar_file_id} style={{ height: '40px', width: '40px' }} />
+            </div>
+            <div className={styles.comment_content_wrapper}>
+                <div className={styles.comment_content}>
+                    <h5>{comment.user.full_name}</h5>
+                    <p>{comment.content}</p>
+                </div>
+                <div className={styles.comment_content_bottom}>
+                    <span>1h</span>
+                    <span>Like</span>
+                    <span>Dislike</span>
+                    <span>Reply</span>
+                </div>
+            </div>
         </div>
     );
 }
