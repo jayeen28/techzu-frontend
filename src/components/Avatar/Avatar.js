ca86fa409950da7112443e74a28433d87af1e05f
jayeen28 <jayeen6783@gmail.com>
2024-07-13 20:32:23 +0600

    default image loaded for avatar

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -2,9 +2,17 @@ import React from 'react';
 import styles from './avatar.module.scss';
 
 const Avatar = ({ id, ...rest }) => {
+
     return (
         <div className={styles.avatar_wrapper} {...rest}>
-            <img className={styles.avatar} src={`${process.env.REACT_APP_BACKEND_URL}/file/${id}`} alt='Avatar' />
+            <img
+                className={styles.avatar}
+                src={id ? `${process.env.REACT_APP_BACKEND_URL}/file/${id}` : 'https://www.w3schools.com/howto/img_avatar.png'} alt='Avatar'
+                onError={({ currentTarget }) => {
+                    currentTarget.onerror = null; // prevents looping
+                    currentTarget.src = 'https://www.w3schools.com/howto/img_avatar.png';
+                }}
+            />
         </div>
     );
 }
